1aa9fb21419f6a58c5f8c37fbfb7607fc4443433
LanaSST <132131525+LanaSST@users.noreply.github.com>
2026-03-15 15:02:41 +0300

    Add files via upload

---
+++ Source14.2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int main()
+{
+    struct Account
+    {
+        int nNumber{0};
+        std::string strName;
+        double dBalance{};
+
+        void setNewBalance(Account & acc, double dNewBalance)
+        {
+            acc.dBalance = dNewBalance;
+        }
+
+        void print()
+        {
+            std::cout << "Ваш счёт: " << strName << ", " << nNumber << ", " << dBalance << std::endl;
+        }
+    };
+
+    Account acc;
+    double dNewBalance{};
+    std::cout << "Введите номер счёта: ";
+    std::cin >> acc.nNumber;
+    std::cout << std::endl;
+    std::cout << "Введите имя владельца: ";
+    std::cin >> acc.strName;
+    std::cout << std::endl;
+    std::cout << "Введите баланс: ";
+    std::cin >> acc.dBalance;
+    std::cout << "Введите новый баланс: ";
+    std::cin >> dNewBalance;
+    std::cout << std::endl;
+    acc.setNewBalance(acc, dNewBalance);
+    acc.print();
+
+    return 0;
+}
