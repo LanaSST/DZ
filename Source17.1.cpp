879b8e385968f094c8bcad1d2e6118f4cb7a9a4a
LanaSST <132131525+LanaSST@users.noreply.github.com>
2026-03-29 18:35:50 +0300

    Add files via upload

---
+++ Source17.1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+class Figure
+{
+private:
+    int nCountSide{};
+    std::string name;
+
+public:    
+    Figure(int nCountSide_ = 0, std::string_view name_ = "Фигура") : nCountSide(nCountSide_), name(name_)
+    { }
+
+    int getCountSide()
+    {
+        return nCountSide;
+    }
+
+    std::string getName()
+    {
+        return name;
+    }
+};
+
+class Triangle: public Figure
+{
+public: 
+    Triangle() : Figure(3, "Треугольник")
+    { }
+};
+
+class Quadrangle: public Figure
+{
+public: 
+    Quadrangle() : Figure(4, "Четырёхугольник")
+    { }
+};
+
+
+int main()
+{
+    Figure figure;
+    Triangle triangle;
+    Quadrangle quadrangle;
+
+    std::cout << "Количество сторон:" << std::endl;
+    std::cout << figure.getName() << ": " << figure.getCountSide() << std::endl;
+    std::cout << triangle.getName() << "Треугольник: " << triangle.getCountSide() << std::endl;
+    std::cout << quadrangle.getName() << ": " << quadrangle.getCountSide() << std::endl;
+    return 0;
+}
