512e15e846b0a451aef2ecec8102cbdc92d653dc
LanaSST <132131525+LanaSST@users.noreply.github.com>
2026-04-05 14:51:20 +0300

    Add files via upload
    макросы и препроцессоры

---
+++ Source19.1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#define MODE 1
+
+#ifndef MODE
+#error Режим не определен
+#elif MODE == 1
+    double add(double First, double Second)
+    {
+        return First + Second;
+    }
+#endif
+
+int main()
+{
+#if MODE == 0
+    std::cout <<  "Работаю в режиме тренировки" << std::endl;
+#elif MODE == 1
+    double first{};
+    double second{};
+    std::cout <<  "Работаю в боевом режиме" << std::endl;
+    std::cout <<  "Введите число 1: ";
+    std::cin >> first;
+    std::cout <<  "Введите число 2: ";
+    std::cin >> second;
+    std::cout <<  "Результат сложения: " << add(first, second) << std::endl;
+#else
+    std::cout <<  "Неизвестный режим. Завершение работы" << std::endl;
+#endif
+    return 0;
+}
